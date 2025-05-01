4cfdf5c77ab645ea1b8bb5bb03280301aa9b40a6
momokoboi <72919896+momokoboi@users.noreply.github.com>
2025-05-02 01:38:32 +0200

    paths

--- docs/Scripts/script.js
+++ docs/Scripts/script.js
@@ -13,7 +13,7 @@ let index_burger = 0
 let burgers_array = []
 async function fetchData(){
     try {
-        const response = await fetch('Brugers/docs/Json_files/burgers.json');
+        const response = await fetch('../Json_files/burgers.json');
         const data = await response.json();
   
         data.forEach((burger)=>{
